af0b7ae919eb6243df9fc87b6e70af419e9a8d9c
Shivkumarmedar <121854125+Shivkumarmedar@users.noreply.github.com>
2023-07-25 19:05:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  return num.toString(2)
 }
-
+const output = arr.map(decimalToBinary)
+console.log(output)
 window.decimalToBinary = decimalToBinary;
